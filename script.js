edfb5f34d7d502e62176cd732d4d644d5a401769
Darren Sisson <darrensisson@hotmail.com>
2024-03-23 12:38:49 +0000

    fix scrolling, add char selector with colour changes

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 document.querySelectorAll("li").forEach((item) => {
   item.addEventListener("click", (event) => {
     window.location.hash = item.getAttribute("jumpto");
-    document.querySelectorAll("li").forEach((listitem) => {
-        listitem.setAttribute("active","false");
-    });
-    item.setAttribute("active","true");
+    document.querySelector("li[active='true']").setAttribute("active", "false");
+    item.setAttribute("active", "true");
   });
 });
 
 var allImages = document.getElementsByTagName("img");
-    for (element of allImages) { 
-        element.draggable = false;
-    }
+for (element of allImages) {
+  element.draggable = false;
+}
+
+window.addEventListener("resize", (event) => {
+  scrollTo({
+    top: document.getElementById(document.querySelector("li[active='true']").getAttribute("jumpto")).offsetTop,
+    left: 0,
+    behaviour: "instant",
+  });
+});
+
+
+document.querySelectorAll(".icon").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    document.querySelector(`.icon[region='${item.getAttribute("region")}'][active=true]`).setAttribute("active", "false");
+    item.setAttribute("active", "true");
+  })
+})
